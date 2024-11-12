e562d9a6967bf2c5f44157733580de3bcbaee300
DaryaVasiliuk <dashka.chernichka14@gmail.com>
2024-11-12 22:10:15 +0300

    feat: decide some  exercise

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -356,8 +356,13 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .map(function reverse(rev) {
+      return rev.split('').reverse().join('');
+    })
+    .join(' ');
 }
 
 /**
@@ -388,8 +393,12 @@ function invertCase(/* str */) {
  *   getStringFromTemplate('John','Doe') => 'Hello, John Doe!'
  *   getStringFromTemplate('Chuck','Norris') => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  const hello = 'Hello';
+  const comma = ',';
+  const space = ' ';
+  const mark = '!';
+  return `${hello}${comma}${space}${firstName}${space}${lastName}${mark}`;
 }
 
 /**
@@ -402,8 +411,12 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const match = value.match(/Hello, (.+)!/);
+  if (match) {
+    return match[1];
+  }
+  return '';
 }
 
 /**
@@ -417,8 +430,8 @@ function extractNameFromTemplate(/* value */) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.slice(1, -1);
 }
 
 /**
